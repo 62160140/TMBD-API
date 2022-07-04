1e926996ebb029f3ef149bcae8a2bd7f783d47d6
Pakawat Charoenying <62160140@go.buu.ac.th>
2022-07-04 12:51:05 +0700

    fetch movie by dropdawn

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const apiKey = 'c311ea48d602ea0dc748177ff03a3d86'
-const year = 2020
+let year = 2020
 const url = `https://api.themoviedb.org/3/discover/movie?api_key=
 ${apiKey}&language=en-US&sort_by=popularity.desc&page=1&year=${year}&with_watch_monetization_types=flatrate`
 const content = document.getElementById('content')
 const urlPoster= 'https://image.tmdb.org/t/p/w500/'
+const dropdown = document.getElementById('years')
+
 
 async function displayMovies(url){
     const  res =  await fetch(url);
     const data = await res.json()
-    console.log(data);
+
+      // clear contentEl
+    clearContentEl()    
+    
     data.results.forEach(element => {
       const movieEl = document.createElement('div')
+      movieEl.classList.add('movie')
       const title = document.createElement('h2')
       const poster = document.createElement('img')
       poster.src = `${urlPoster+element.poster_path}`
 
 
-      title.innerHTML = `${element.title}`
+      title.innerHTML = `${element.title.substring(0,24)}`
       movieEl.appendChild(title)
       movieEl.appendChild(poster)
 
@@ -24,4 +30,19 @@ async function displayMovies(url){
     });
 }
 
+dropdown.addEventListener('change',(e)=>{
+  year=e.target.value
+  const updateUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=1&year=${year}&with_watch_monetization_types=flatrate`
+  // console.log(updateUrl);
+  displayMovies(updateUrl
+    )
+})
+
+
 displayMovies(url)
+
+function clearContentEl() {
+  while (content.firstChild) {
+    content.removeChild(content.firstChild)
+  }
+}
